27ed83f6acc72cde984ecf16fe4b1edeaab6af4c
Ibrahim <maulanayusuf5579@gmail.com>
2025-12-22 13:36:22 +0700

    Create local clusterIssuer deployment

--- k8s-deployments/cert-manager/1-install.sh
+++ k8s-deployments/cert-manager/1-install.sh
@@ -8,3 +8,6 @@ gpg --export $HOME/.gnupg/pubring.gpg
 rm -v cert-manager-pgp-2021-09-20-1020CF3C033D4F35BAE1C19E1226061C665DF13E.asc
 
 helm install cert-manager jetstack/cert-manager -n cert-manager --create-namespace --set crds.enabled=true --verify
+
+printf "\n[!] Creating new secret to store local Root CA...\n"
+kubectl create secret tls ibrahim-root-ca -n cert-manager --cert=private/root-ca.crt --key=private/root-ca.key
